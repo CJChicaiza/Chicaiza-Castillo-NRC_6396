e3b5350aa5f745bc691a14d15653c90eaeffb161
CJChicaiza <cjchicaiza2@espe.edu.ec>
2020-06-06 16:06:52 -0500

    .

--- Taylor Series/Unit Test/CosSinUnitTest/test.cpp
+++ Taylor Series/Unit Test/CosSinUnitTest/test.cpp
@@ -1,25 +1,66 @@
+/******************************************************************
+*            UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE              *
+* CARRERA: Ingenieria de Software                                 *
+* SEMESTRE: Tercer semestre                                       *
+* MATERIA: Estructura de Datos                                    *
+* NRC: 6396                                                       *
+* AUTOR: Chicaiza Jaya Cristopher Jennsy                          *
+* VERSION DEL PROGRAMA: 1.0                                       *
+******************************************************************/
+
+/* Se realizan 10 pruebas con diferentes datos*/
 #include "pch.h"
+#include "ProcCosSin.h"
 
 #define MAX 999
 #define PI 3.1415926535897
-float Sin(float x, int n)
-{
-	if (n > MAX)
-		return x;
-	return x - x * x / (((2 * n + 1) - 1) * (2 * n + 1)) * Sin(x, n + 1);
-}
-
-float Cosin(float x, int n)
-{
-	if (n > MAX)
-		return 1;
-	return 1 - x * x / ((2 * n - 1) * 2 * n) * Cosin(x, n + 1);
-}
-TEST(TestSin, TestPI) {
-	EXPECT_EQ(Sin(PI, 1), 0);
+TEST(TestSin1, TestPI) {
+	ProcCosSin objProc;
+	EXPECT_EQ(objProc.Sin(PI, 1), 0);
 	EXPECT_TRUE(true);
 }
-TEST(TestCos, TestPI) {
-	EXPECT_EQ(Cosin(PI, 1), -1);
+TEST(TestCos1, TestPI) {
+	ProcCosSin objProc;
+	EXPECT_EQ(objProc.Sin(PI, 1), -1);
+	EXPECT_TRUE(true);
+}
+TEST(TestSin2, TestPI) {
+	ProcCosSin objProc;
+	EXPECT_EQ(objProc.Sin(0, 1), 0);
+	EXPECT_TRUE(true);
+}
+TEST(TestCos2, TestPI) {
+	ProcCosSin objProc;
+	EXPECT_EQ(objProc.Sin(0, 1), 1);
+	EXPECT_TRUE(true);
+}
+TEST(TestSin3, TestPI) {
+	ProcCosSin objProc;
+	EXPECT_EQ(objProc.Sin(PI/2, 1), 1);
+	EXPECT_TRUE(true);
+}
+TEST(TestCos3, TestPI) {
+	ProcCosSin objProc;
+	EXPECT_EQ(objProc.Sin(PI/2, 1), 0);
+	EXPECT_TRUE(true);
+}
+TEST(TestSin4, TestPI) {
+	ProcCosSin objProc;
+	EXPECT_EQ(objProc.Sin(PI*2, 1), 0);
+	EXPECT_TRUE(true);
+}
+TEST(TestCos4, TestPI) {
+	ProcCosSin objProc;
+	EXPECT_EQ(objProc.Sin(PI*2, 1), 1);
+	EXPECT_TRUE(true);
+}
+TEST(TestSin5, TestPI) {
+	ProcCosSin objProc;
+	EXPECT_EQ(objProc.Sin(PI*3/2, 1), -1);
+	EXPECT_TRUE(true);
+}
+TEST(TestCos5, TestPI) {
+	ProcCosSin objProc;
+	EXPECT_EQ(objProc.Sin(PI*3/2, 1), 0);
 	EXPECT_TRUE(true);
 }
